3d1f8f7a40a96efca0891fabf58a0e47544d921e
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-10-29 19:47:54 +0800

    add basic math operators functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function addNumbers(a, b){
+    return a + b;
+}
+
+function subtractNumbers(a, b){
+    return a - b;
+}
+
+function multiplyNumbers(a, b){
+    return a * b;
+}
+
+function divideNumbers(a, b) {
+    return a / b;
+}
+
+
+console.log(addNumbers(3, 5))
+console.log(subtractNumbers(3, 5))
+console.log(multiplyNumbers(3, 5))
+console.log(divideNumbers(3, 5))
