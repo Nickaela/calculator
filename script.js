c53c528f16442b27062257619876439592f95aed
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-11-04 11:16:25 +0800

    no idea what i'm doing

--- script.js
+++ script.js
@@ -31,52 +31,51 @@ function operate(){
     }
  };
 
+
+
 const display = document.querySelector("p");
+const buttons = Array.from(document.querySelectorAll(".number"));
+const operators = Array.from(document.querySelectorAll(".op"));
+const another = Array.from(document.querySelectorAll(".number"));
 
-const button1 = document.querySelector(".num1")
-const button2 = document.querySelector(".num2")
-const addd = document.querySelector(".numAdd")
-const result = document.querySelector(".numEquals");
 
-function one() {
-button1.addEventListener("click", () => {
-    display.textContent = button1.innerText;
-    const first = display.textContent;
-    firstNumber = first;
-    console.log(firstNumber);
-})
+
+
+
+function first(){
+for (const button of buttons){
+    button.addEventListener("click", () => {
+        display.textContent = button.innerText;
+        const first = display.textContent;
+        firstNumber = first;
+        console.log(firstNumber)
+    })}
+}
+first()
+
+
+function second(){
+for (const anot of another){
+    anot.addEventListener("click", () => {
+        display.textContent = anot.innerText;
+        const second = display.textContent;
+        secondNumber = second;
+        console.log(secondNumber)
+    })}
 }
 
-one();
 
-function add() {
-    addd.addEventListener("click", () => {
-        display.textContent = firstNumber;
-        operator = addd.innerText;
-        console.log(operator)
-        second();
-    })
+function forTheOperate(){
+    for (const op of operators){
+        op.addEventListener("click", () => {
+            display.textContent = firstNumber;
+            operator = op.innerText;
+            console.log(operator)
+            second();
+        })
+    }
+
+   
 }
 
-add();
-
-function second() {
-    button2.addEventListener("click", () => {
-    display.textContent = button2.innerText;
-    const second = display.textContent;
-    secondNumber = second;
-    console.log(secondNumber);
-})
-}
-
-function resultSum() {
-    result.addEventListener("click", () => {
-    display.textContent = button2.innerText;
-    console.log(firstNumber)
-    console.log(secondNumber)
-    console.log(operator)
-    console.log(operate());
-})
-}
-
-resultSum();
+forTheOperate();
